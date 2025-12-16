4dba1af62cc9bdbc340fb907c40aa024446a915d
STS-Engineer <administration.sts@avocarbon.com>
2025-12-16 10:44:53 +0100

    add pagination

--- src/pages/documents/Documents.tsx
+++ src/pages/documents/Documents.tsx
@@ -1,16 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
 import documentIcon from "../../icons/excelIcon.svg";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getDocuments } from "../../redux/documents/document";
 import { Document } from "../../redux/documents/document-slice-types";
+import Pagination from "../../components/common/Pagination";
 
 const Documents = () => {
   const dispatch = useDispatch();
   const { documentsList } = useSelector((state: any) => state.document);
+  const [page, setPage] = useState(1);
+  const per_page = 5;
+
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+  };
 
   useEffect(() => {
-    getDocuments(dispatch);
-  }, [dispatch]);
+    getDocuments(dispatch, page, per_page);
+  }, [dispatch, page, per_page]);
 
   return (
     <div className="p-6">
@@ -27,7 +34,7 @@ const Documents = () => {
           bg-transparent
         "
       >
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 justify-items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6 justify-items-center">
           {documentsList?.items?.length === 0 ? (
             <div className="col-span-full flex items-center justify-center h-48">
                 <span className="text-xl text-gray-400 italic text-center">
@@ -58,6 +65,11 @@ const Documents = () => {
             )}
         </div>
       </div>
+       <Pagination
+          page={documentsList.page}
+          totalPages={documentsList.total_pages}
+          onPageChange={handlePageChange}
+        />
     </div>
   );
 };
